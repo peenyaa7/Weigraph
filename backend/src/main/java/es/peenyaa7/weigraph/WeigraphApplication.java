3d0c1002ab879998c0fd1389873f8da8b6d96511
Francisco Javier Peña Vela <peenyaa7@gmail.com>
2025-06-10 22:53:01 +0200

    feat: Add Spring Boot project working with SQLite

---
+++ backend/src/main/java/es/peenyaa7/weigraph/WeigraphApplication.java
@@ -0,0 +1,13 @@
+package es.peenyaa7.weigraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeigraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeigraphApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/es/peenyaa7/weigraph/WeigraphApplicationTests.java
@@ -0,0 +1,13 @@
+package es.peenyaa7.weigraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeigraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
