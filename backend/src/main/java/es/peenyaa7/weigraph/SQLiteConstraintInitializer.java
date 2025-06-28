d5cb0bf9b71c16c6dd24f28dff0361fd4369f7e0
Francisco Javier Peña Vela <peenyaa7@gmail.com>
2025-06-28 19:47:04 +0200

    refactor: Refactor SQLiteConstraintInitializer

--- backend/src/main/java/es/peenyaa7/weigraph/SQLiteConstraintInitializer.java
+++ backend/src/main/java/es/peenyaa7/weigraph/SQLiteConstraintInitializer.java
@@ -1,6 +1,7 @@
 package es.peenyaa7.weigraph;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
@@ -8,6 +9,10 @@ import javax.sql.DataSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * SQLite doesn't support @UniqueContraint annotation in the entity class.
+ * For this reason, we need to create the index manually.
+ */
 @Component
 public class SQLiteConstraintInitializer implements CommandLineRunner {
     private final DataSource dataSource;
@@ -18,6 +23,10 @@ public class SQLiteConstraintInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        createDateAndUserIdUniqueContraintOnWeightEntriesTable();
+    }
+    
+    private void createDateAndUserIdUniqueContraintOnWeightEntriesTable() throws SQLException {
         try (Connection conn = dataSource.getConnection();
             Statement stmt = conn.createStatement()) {
             stmt.execute("CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_date_user ON weight_entries(date, user_id);");
