721bacf0674655eaf95e024bc5eedd474521d7ab
Francisco Javier Peña Vela <peenyaa7@gmail.com>
2025-07-27 14:39:17 +0200

    feat: Add styles to profile view

--- frontend/src/views/ProfileView/ProfileView.tsx
+++ frontend/src/views/ProfileView/ProfileView.tsx
@@ -5,17 +5,28 @@ export const ProfileView = () => {
 
     const { profile, loading } = useProfile();
 
-    return (
-        <div>
-            {
-                loading ? <p>Loading...</p> :
-                    profile &&
-                    <div>
-                        <p>{profile.id}</p>
-                        <p>{profile.username}</p>
-                        <p>{profile.weightGoal}</p>
-                    </div>
-            }
-        </div>
+    return (<>
+        <h1 className="text-4xl">Tu perfil</h1>
+        {
+            loading ? <div className="skeleton h-96 w-3xl"></div> : (
+                profile ? (
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th>Usuario</th>
+                                <td>{profile.username}</td>
+                            </tr>
+                            <tr>
+                                <th>Peso objetivo</th>
+                                <td>{profile.weightGoal} kg</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>No hay perfil</p>
+                )
+            )
+        }
+    </>
     )
 }
